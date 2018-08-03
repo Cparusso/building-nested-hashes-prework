98b86e79f64314155f58e1555c1f8cf37a6a8e3e
Charlie Russo <cparusso@gmail.com>
2018-08-03 19:18:38 +0000

    automatically backed up by learn

--- lib/fourth_challenge.rb
+++ lib/fourth_challenge.rb
@@ -16,7 +16,7 @@ def fourth_challenge
       },
       hero_friends: [
         {name: "Benvolio", age: "17", attitude: "worried"},
-        {name: "", age: "", attitude: ""}
+        {name: "Mercutio", age: "18", attitude: "hot-headed"}
       ]
     },
     capulet: {

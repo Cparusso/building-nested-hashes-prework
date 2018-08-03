d2484059ba496b4c97ec76f2d1537b2ebe27a6e7
Charlie Russo <cparusso@gmail.com>
2018-08-03 19:02:31 +0000

    automatically backed up by learn

--- lib/first_challenge.rb
+++ lib/first_challenge.rb
@@ -4,7 +4,10 @@ def first_challenge
       patriarch: {
         name: "Lord Montague"
         age: "53"
+      },
+      matriarch: {
+        name: "Lady Montague"
+        age: "54"
       }
-        ,
    }
 end

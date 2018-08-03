cbdb250e9eebb01dd9336c425c1be8c35dd294c1
Charlie Russo <cparusso@gmail.com>
2018-08-03 19:06:48 +0000

    automatically backed up by learn

--- lib/first_challenge.rb
+++ lib/first_challenge.rb
@@ -1,26 +1,6 @@
 def first_challenge
    epic_tragedy = {
-     montague: {
-      patriarch: {
-        name: "Lord Montague"
-        age: "53"
-      },
-      matriarch: {
-        name: "Lady Montague"
-        age: "54"
-      },
-      hero: {
-        name: "Romeo"
-        age: "17"
-      },
-      hero_friends: {
-        [{
-          name: "Mercutio"
-          age:
-          attitude: "worried"
-        }
-        ]
-      }
-      }
+     montague: {}
+     capulet: {}
    }
 end

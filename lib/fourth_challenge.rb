c32c7651049517dc06b2d6416d79d97688325a19
Charlie Russo <cparusso@gmail.com>
2018-08-03 19:18:24 +0000

    automatically backed up by learn

--- lib/fourth_challenge.rb
+++ lib/fourth_challenge.rb
@@ -15,7 +15,7 @@ def fourth_challenge
         status: "alive"
       },
       hero_friends: [
-        {name: "", age: "", attitude: ""},
+        {name: "Benvolio", age: "17", attitude: "worried"},
         {name: "", age: "", attitude: ""}
       ]
     },
